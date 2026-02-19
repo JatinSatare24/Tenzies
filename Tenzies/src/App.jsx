4b2fd2c35ced5d2021f2a6563aff80819c0570ea
Jatin Satare <jatinsatare@gmail.com>
2026-02-19 14:41:38 +0530

    add Confetti & react import to the app

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice())
